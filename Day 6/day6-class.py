1441ff7f4f3020a4deedf6d883a0956b2793ff84
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-01-07 14:15:43 -0300

    day 6 of 100

---
+++ Day 6/day6-class.py
@@ -0,0 +1,16 @@
+'''
+Como criar funções em python utilizando a cláusula "def".
+Como utilizar a estrutura de repetição "while"
+'''
+
+
+def my_function(name):
+    print(f'Olá {name}')
+
+my_function('Daniel')
+
+
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1
