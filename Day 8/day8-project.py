13b002b5f4a3992f95e8d0f5106261b05dbf9117
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-01-10 19:18:48 -0300

    day 8 of 100

--- Day 8/day8-project.py
+++ Day 8/day8-project.py
@@ -14,57 +14,38 @@ alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 palavra = input('Digite a sua mensagem:\n').lower()
 numero = int(input('Escolha o número chave\n'))
+opcao = int(input('Para ecriptar essa mensagem, digite "1". Para decriptar, digite "2"\n'))
 
+def caesar(msg=palavra, num=numero, opc=opcao):
 
-palavras = palavra.split(' ')
+    palavra_resultante = ''
 
-def encripta(encriptando=palavras):
-
-    palavra_encriptada = ''
-
-    for palavra in palavras:
-        for letra in palavra:
+    for caractere in palavra:
+        if caractere in alfabeto:
             for iter in range(len(alfabeto)):
-                if letra == alfabeto[iter]:
-                    recomeca = (iter + numero) - 26
-                    if recomeca >= 0:
-                        letra_encriptada = alfabeto[recomeca]
-                        palavra_encriptada += letra_encriptada
+                if caractere == alfabeto[iter]:
+                    if opcao == 1:
+                        indice = (iter + numero) % 26
+                        palavra_resultante += alfabeto[indice]
+                    elif opcao == 2:
+                        indice = (iter - numero) % 26
+                        palavra_resultante += alfabeto[indice]
                     else:
-                        letra_encriptada = alfabeto[iter + numero]
-                        palavra_encriptada += letra_encriptada
+                        print(f'Escolha inválida. Inicie o programa novamente.')
+        else:
+            palavra_resultante += caractere
 
-        if len(palavras) >= 2:
-            palavra_encriptada += ' '
-
-    print(palavra_encriptada)
-
-def decripta(decriptando=palavras):
-
-    palavra_decriptada = ''
-
-    for palavra in palavras:
-        for letra in palavra:
-            for iter in range(len(alfabeto)):
-                if letra == alfabeto[iter]:
-                    letra_decriptada = alfabeto[iter - numero]
-                    palavra_decriptada += letra_decriptada
-
-        if len(palavras) >= 2:
-            palavra_decriptada += ' '        
-
-    print(palavra_decriptada)
+    print(f'{palavra_resultante}\n')
 
 
+caesar()
 
-opcao = int(input('Para encriptar essa mensagem, digite "1". Para decriptar, digite "2"\n'))
-while opcao != 1 or opcao != 2:
-    if opcao == 1:
-        encripta(encriptando=palavras)
-        break
-    elif opcao == 2:
-        decripta(decriptando=palavras)
-        break
-    else:
-        print(f'A opção {opcao} não está disponível. Tente novamente.')
-        opcao = int(input('Para encriptar essa mensagem, digite "1". Para decriptar, digite "2"\n'))
+continua = bool(int(input('Deseja digitar outra mensagem? Digite "1" para sim, "0" para não.\n')))
+
+while continua:
+    if continua:
+        palavra = input('Digite a sua mensagem:\n').lower()
+        numero = int(input('Escolha o número chave\n'))
+        opcao = int(input('Para ecriptar essa mensagem, digite "1". Para decriptar, digite "2"\n'))
+        caesar()
+        continua = bool(int(input('Deseja digitar outra mensagem? Digite "1" para sim, "0" para não.\n')))
