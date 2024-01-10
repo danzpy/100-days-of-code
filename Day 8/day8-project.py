2d531c277f5622aeccee4ac3a74ae788ee15583e
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-01-09 21:07:58 -0300

    day 8 of 100

--- Day 8/day8-project.py
+++ Day 8/day8-project.py
@@ -1,3 +1,15 @@
+'''
+Cifra de César
+
+Em criptografia, a Cifra de César, também conhecida como cifra de troca,
+código de César ou troca de César, é uma das mais simples e conhecidas
+técnicas de criptografia. É um tipo de cifra de substituição na qual
+cada letra do texto é substituída por outra, que se apresenta no
+alfabeto abaixo dela um número fixo de vezes. Por exemplo,
+com uma troca de três posições, A seria substituído por
+D, B se tornaria E, e assim por diante.
+'''
+
 alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 palavra = input('Digite a sua mensagem:\n').lower()
