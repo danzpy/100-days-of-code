c4c69b3b5aae795908dd9672a5b5ad4faf151059
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-01-09 21:21:07 -0300

    day 4 of 100

--- Day 4/day4-project.py
+++ Day 4/day4-project.py
@@ -1,36 +1,52 @@
+'''
+Pedra, papel e tesoura
+
+Também chamado em algumas regiões do Brasil de jokenpô,
+é um jogo de mãos recreativo e simples para duas ou mais
+pessoas, que não requer equipamentos nem habilidade.
+
+Nesse jogo, os jogadores devem simultaneamente esticar a mão,
+na qual cada um formou um símbolo (que significa pedra, papel ou tesoura).
+Então, os jogadores comparam os símbolos para decidir quem ganhou, da seguinte forma:
+
+Pedra ganha da tesoura (amassando-a ou quebrando-a).
+Tesoura ganha do papel (cortando-o).
+Papel ganha da pedra (embrulhando-a).
+'''
+
 from random import randint
 
-random_option = randint(0, 2)
-computer_choice_str = None
-computer_choice_number = 'string'
+opcao_aleatoria = randint(0, 2)
+escolha_computador_str = None
+escolha_computador_num = ''
 
-your_choice = int(input('Vamos jogar "Jokenpô". O que você escolhe? 0 para "Pedra", 1 para "Papel" e 2 para "Tesoura"\n'))
-your_choice_str = 'string'
+sua_escolha = int(input('Vamos jogar "Jokenpô". O que você escolhe? 0 para "Pedra", 1 para "Papel" e 2 para "Tesoura"\n'))
+sua_escolha_str = 'string'
 
-if your_choice == 0:
-    your_choice_str = 'Pedra'
-elif your_choice == 1:
-    your_choice_str = 'Papel'
-elif your_choice == 2:
-    your_choice_str = 'Tesoura'
+if sua_escolha == 0:
+    sua_escolha_str = 'Pedra'
+elif sua_escolha == 1:
+    sua_escolha_str = 'Papel'
+elif sua_escolha == 2:
+    sua_escolha_str = 'Tesoura'
 else:
-    print(f'Você digitou {your_choice}, valor indevido.')
+    print(f'Você digitou {sua_escolha}, valor indevido.')
 
-if random_option == 0:
-    computer_choice_str = 'Pedra'
-    computer_choice_number = 0
-elif random_option == 1:
-    computer_choice_str = 'Papel'
-    computer_choice_number = 1
+if opcao_aleatoria == 0:
+    escolha_computador_str = 'Pedra'
+    escolha_computador_num = 0
+elif opcao_aleatoria == 1:
+    escolha_computador_str = 'Papel'
+    escolha_computador_num = 1
 else:
-    computer_choice_str = 'Tesoura'
-    computer_choice_number = 2
+    escolha_computador_str = 'Tesoura'
+    escolha_computador_num = 2
 
-if (your_choice == 0 and computer_choice_number == 1) or (your_choice == 1 and computer_choice_number == 0) or (your_choice == 2 and computer_choice_number == 1):
-    print(f'Você escolheu {your_choice_str} e o computador escolheu {computer_choice_str}. Parabéns, você ganhou!')
-elif your_choice == random_option:
-    print(f'Você escolheu {your_choice_str} e o computador escolheu {computer_choice_str}. Ninguém ganhou!')
-elif your_choice > 2 or your_choice < 0:
+if (sua_escolha == 0 and escolha_computador_num == 1) or (sua_escolha == 1 and escolha_computador_num == 0) or (sua_escolha == 2 and escolha_computador_num == 1):
+    print(f'Você escolheu {sua_escolha_str} e o computador escolheu {escolha_computador_str}. Parabéns, você ganhou!')
+elif sua_escolha == opcao_aleatoria:
+    print(f'Você escolheu {sua_escolha_str} e o computador escolheu {escolha_computador_str}. Ninguém ganhou!')
+elif sua_escolha > 2 or sua_escolha < 0:
     print('Jogue novamente.')
 else:
-    print(f'Você escolheu {your_choice_str} e o computador escolheu {computer_choice_str}. Você perdeu!')-1
+    print(f'Você escolheu {sua_escolha_str} e o computador escolheu {escolha_computador_str}. Você perdeu!')-1
