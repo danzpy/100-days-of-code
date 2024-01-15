c6ed471c173fadf4f10b17cdf78a92f0de715407
Daniel L. Alves <daniel.leonssio@hotmail.com>
2024-01-15 19:00:19 -0300

    Ajuste de funções. Agora as cartas estão sendo adicionadas corretamente

--- Day 11/functions.py
+++ Day 11/functions.py
@@ -1,6 +1,18 @@
 from random import choice
 
+cartas_jogador = []
+cartas_maquina = []
 
+def armazena_cartas(inicio=True):
+
+    if inicio:
+        for i in range(2):
+            cartas_jogador.append(seleciona_cartas())
+        cartas_maquina.append(seleciona_cartas())
+    else:
+        cartas_maquina.append(seleciona_cartas())
+        cartas_jogador.append(seleciona_cartas())
+            
 def seleciona_cartas():
     '''
     Seleciona cartas de forma "randômica" para os jogadores.
@@ -11,43 +23,33 @@ def seleciona_cartas():
     Essa amostragem será com reposição, presumindo que em cassinos
     mais de uma baralho são utilizados.
     '''
-
-    rodada = 1
-
     cartas = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-    cartas_jogador = []
-    cartas_maquina = []
+    carta_selecionada = choice(cartas)
 
-    if rodada == 1:
-        for i in range(2):
-            cartas_jogador.append(choice(cartas))
-        cartas_maquina.append(choice(cartas))
-    else:
-        cartas_jogador.append(choice(cartas))
-        cartas_maquina.append(choice(cartas))
-        rodada += 1
+    return carta_selecionada
 
-    return cartas_jogador, cartas_maquina, rodada
-
-def mostra_cartas(cartas_jogador, cartas_maquina, rodada):
-    if rodada == 1:
+def mostra_cartas(cartas_jogador, cartas_maquina, inicio=True):
+    if inicio:
         print(f'Suas cartas são: {cartas_jogador}')
         print(f'A primeira carta da máquina é: {cartas_maquina}')
-        rodada += 1
     else:
         print(f'Suas cartas são: {cartas_jogador}')
-        print(f'A mão da máquina é: {cartas_maquina}')     
-    print(f'A rodada no mostra cartas é {rodada}')
+        print(f'A mão da máquina é: {cartas_maquina}')
 
-def continua_jogando(cartas_jogador, cartas_maquina, rodada):
 
-    continua = True
+def acaba_jogo():
+
+    agradecimento = print('Obrigado por jogar Blackjack!')
+
+    return agradecimento
+
+def continua_jogando(cartas_jogador, cartas_maquina):
 
     opcao = input('Deseja continuar jogando? Digite "s" para sim, ou "n" para não.\n')
-    if opcao == 's':
-        
-        mostra_cartas(cartas_jogador, cartas_maquina, rodada)
-        print(f'A rodada no continua jogando é {rodada}')
-    
-    return continua
+    while opcao == 's':
+        armazena_cartas(inicio=False)
+        mostra_cartas(cartas_jogador, cartas_maquina, inicio=False)
+        opcao = input('Deseja continuar jogando? Digite "s" para sim, ou "n" para não.\n')
+    if opcao == 'n':
+        acaba_jogo()
--- Day 11/main.py
+++ Day 11/main.py
@@ -4,15 +4,19 @@ from art import logo
 import functions
 
 def blackjack():
+
+    cartas_jogador = []
+    cartas_maquina = []
+
     inicio = input('Você gostaria de iniciar uma partida de Blackjack agora? Digite "s" para iniciar, ou "n" para encerrar a aplicação.\n').lower()
 
     if inicio == 'n':
         print('Aplicação encerrada.')
     elif inicio == 's':
         print(logo)
-        cartas_jogador, cartas_maquina, rodada = functions.seleciona_cartas()
-        functions.mostra_cartas(cartas_jogador, cartas_maquina, rodada)
-        functions.continua_jogando(cartas_jogador, cartas_maquina, rodada)
+        functions.armazena_cartas(inicio=True)
+        functions.mostra_cartas(functions.cartas_jogador, functions.cartas_maquina, inicio=True)
+        functions.continua_jogando(functions.cartas_jogador, functions.cartas_maquina)
     else:
         print('Opção inválida.')
         sleep(2)
